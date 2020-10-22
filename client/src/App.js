1bb83e317a5fb7e0cab636e3864747be7aa11e68
Mike VanDenburgh <37340715+mvandenburgh@users.noreply.github.com>
2020-10-21 23:52:40 -0400

    update netlify deployment url in readme

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-
-import StartPage from './components/pages/StartPage';
 import PageFrame from './components/PageFrame';
+import StartPage from './components/pages/StartPage';
+import AtmospherePage from './components/pages/AtmospherePage';
+import DashboardPage from './components/pages/DashboardPage';
+import UserContext from './contexts/UserContext';
+
 
 function App() {
-  // TODO: use something like this later. For now, just set as a boolean.
-  // const [loggedIn, setLoggedIn] = useState(false);
-  const loggedIn = false;
+  const [user, setUser] = useState({
+    username: null,
+    isGuest: true,
+    loggedIn: false,
+  });
 
   return (
     <div className="App">
-      <BrowserRouter>
-          <div className="App">
-            <PageFrame invisible={!loggedIn} />
-            <Switch>
-              <Route exact path="/" component={StartPage} /> {/* TODO: should redirect to dashboard when logged in */}
-            </Switch>
-          </div>
+      <UserContext.Provider value={{user, setUser}}>
+        <BrowserRouter>
+            <PageFrame invisible={!user.isLoggedIn} />
+              <div style={{ position: 'absolute', left: 8*9, height: 'calc(100vh - 8*9)', width: 'calc(100vw - 73px)'}}>
+                <Switch>
+                  <Route exact path="/" component={user.isGuest ? StartPage : DashboardPage} /> {/* TODO: should redirect to dashboard when logged in */}
+                  <Route exact path="/atmosphere" component={AtmospherePage} />
+                </Switch>
+              </div>
         </BrowserRouter>
+      </UserContext.Provider>
     </div>
   );
 }
