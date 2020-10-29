02d58a5bc35be56df49943b292ca14b953a6d6c6
Michael VanDenburgh <michael.vandenburgh@stonybrook.edu>
2020-10-29 00:23:47 -0400

    improve editing mixtape page styling

--- client/src/components/Mixtape.jsx
+++ client/src/components/Mixtape.jsx
@@ -1,7 +1,7 @@
 import React, { useContext, useState } from 'react';
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
-import { AppBar, Button, Grid, Toolbar, Checkbox, List, ListItem, ListItemText } from '@material-ui/core';
-import { blueGray } from '@material-ui/core/colors';
+import { AppBar, Box, Button, Grid, Toolbar, Checkbox, List, ListItem, ListItemText } from '@material-ui/core';
+import { blueGrey } from '@material-ui/core/colors';
 import { Edit as EditIcon, PlayCircleFilledWhite as PlayIcon, Delete as DeleteIcon, AddCircle as AddIcon, Save as SaveIcon } from '@material-ui/icons';
 import CurrentSongContext from '../contexts/CurrentSongContext';
 import PlayingSongContext from '../contexts/PlayingSongContext';
@@ -64,96 +64,107 @@ function Mixtape(props) {
     }
 
     return (
-      <Grid style={{backgroundColor: '#236067', overflow: 'auto', width: '90%', maxHeight: '50vh',}} container justify="center">
-        <DragDropContext onDragEnd={onDragEnd}>
-          <Droppable droppableId='droppable'>
-            {(provided) => (
-              <List
-                {...provided.droppableProps}
-                ref={provided.innerRef}
-                style={{width: '90%'}}
-              >
-                <Toolbar style={{backgroundColor: 'silver', display: enableEditing ? '' : 'none'}}>
-                    {isEditing ?
-                        <Grid container>
-                          <Grid item xs={1}>
-                              <Button startIcon={<SaveIcon />} style={{marginRight: '5%', float: 'right'}} onClick={() => setIsEditing(false)} variant="contained">DONE</Button> 
-                          </Grid>
-                          <Grid item xs={7} />
-                          <Grid item xs={2}>
-                            <Button
-                              style={{marginRight: '5%', float: 'right'}}
-                              variant="contained"
-                              color="secondary"
-                              startIcon={<DeleteIcon />}
-                              onClick={() => deleteSongs()}
-                            >
-                              Delete
-                            </Button>
-                          </Grid>
-                          <Grid item xs={2}>
-                            <Button
-                              style={{marginRight: '5%', float: 'right'}}
-                              variant="contained"
-                              color="primary"
-                              startIcon={<AddIcon />}
-                            >
-                              Add a Song
-                            </Button>
-                          </Grid>
-                        </Grid>
-                      :
-                        <Button startIcon={<EditIcon />} onClick={() => setIsEditing(true)} style={{position: 'absolute', right: '5%'}} variant="contained">EDIT</Button>
-                    }
-                </Toolbar>
-                {mixtape.songs.map((song, index) => (
-                  <Draggable
-                    key={`item${index}`}
-                    draggableId={`item${index}`}
-                    index={index}
-                    isDragDisabled={!isEditing}
-                  >
-                    {(provided, snapshot) => (
-                      // TODO: This list item should be a seperate component
-                      <Grid container>
-                        <ListItem
-                          ref={provided.innerRef}
-                          {...provided.draggableProps}
-                          {...provided.dragHandleProps}
-                          style={getItemStyle(
-                            snapshot.isDragging,
-                            provided.draggableProps.style
-                          )}
-                        >
-                          {isEditing ? 
+      <Box style={{ display: 'inline-flex', 
+                    overflow: 'auto',
+                    maxHeight: '50vh',
+                    flexDirection: 'row', 
+                    backgroundColor: blueGrey[900],
+                    marginBottom: '30px',
+                    paddingTop: '20px',  
+                    paddingBottom: '20px',
+                    width: '90%', 
+                    height: '100%' }} boxShadow={3} borderRadius={12}>
+        <Grid container justify="center">
+          <DragDropContext onDragEnd={onDragEnd}>
+            <Droppable droppableId='droppable'>
+              {(provided) => (
+                <List
+                  {...provided.droppableProps}
+                  ref={provided.innerRef}
+                  style={{width: '90%'}}
+                >
+                  <Toolbar style={{backgroundColor: 'black', display: enableEditing ? '' : 'none'}}>
+                      {isEditing ?
+                          <Grid container>
                             <Grid item xs={1}>
-                              <Checkbox
-                                onClick={() => clickCheckbox(mixtape.songs[index].id)}
-                                checked={songsToDelete.includes(mixtape.songs[index].id)}
-                              />
+                                <Button startIcon={<SaveIcon />} style={{marginRight: '5%', float: 'right'}} onClick={() => setIsEditing(false)} variant="contained">DONE</Button> 
                             </Grid>
-                            : <div />}
-                          <Grid item xs={isEditing ? 9 : 10}>
-                            <div style={{left: '0', marginRight: '10%' }}>
-                              <img style={{width: '70px', height: '70px'}} src={`https://img.youtube.com/vi/${song.id}/1.jpg`} alt='mixtape_cover'></img>
-                              {/* TODO: fetch actual song names from API */}
-                              <ListItemText>{song.name || `song_${mixtape.songs[index]}`}</ListItemText>
-                            </div>
+                            <Grid item xs={7} />
+                            <Grid item xs={2}>
+                              <Button
+                                style={{marginRight: '5%', float: 'right'}}
+                                variant="contained"
+                                color="secondary"
+                                startIcon={<DeleteIcon />}
+                                onClick={() => deleteSongs()}
+                              >
+                                Delete
+                              </Button>
+                            </Grid>
+                            <Grid item xs={2}>
+                              <Button
+                                style={{marginRight: '5%', float: 'right'}}
+                                variant="contained"
+                                color="primary"
+                                startIcon={<AddIcon />}
+                              >
+                                Add a Song
+                              </Button>
+                            </Grid>
                           </Grid>
-                          <Grid item xs={2}>
-                            <PlayIcon fontSize="large" onClick={() => playSong(index)} style={{display: isEditing ? 'none' : '' }} />
-                          </Grid>
-                        </ListItem>
-                      </Grid>
-                    )}
-                  </Draggable>
-                ))}
-                {provided.placeholder}
-              </List>
-            )}
-          </Droppable>
-        </DragDropContext>
-      </Grid>
+                        :
+                          <Button startIcon={<EditIcon />} onClick={() => setIsEditing(true)} style={{position: 'absolute', right: '5%'}} variant="contained">EDIT</Button>
+                      }
+                  </Toolbar>
+                  {mixtape.songs.map((song, index) => (
+                    <Draggable
+                      key={`item${index}`}
+                      draggableId={`item${index}`}
+                      index={index}
+                      isDragDisabled={!isEditing}
+                    >
+                      {(provided, snapshot) => (
+                        // TODO: This list item should be a seperate component
+                        <Grid container>
+                          <ListItem
+                            ref={provided.innerRef}
+                            {...provided.draggableProps}
+                            {...provided.dragHandleProps}
+                            style={getItemStyle(
+                              snapshot.isDragging,
+                              provided.draggableProps.style
+                            )}
+                          >
+                            {isEditing ? 
+                              <Grid item xs={1}>
+                                <Checkbox
+                                  onClick={() => clickCheckbox(mixtape.songs[index].id)}
+                                  checked={songsToDelete.includes(mixtape.songs[index].id)}
+                                />
+                              </Grid>
+                              : <div />}
+                            <Grid item xs={isEditing ? 9 : 10}>
+                              <div style={{left: '0', marginRight: '10%' }}>
+                                <img style={{width: '70px', height: '70px'}} src={`https://img.youtube.com/vi/${song.id}/1.jpg`} alt='mixtape_cover'></img>
+                                {/* TODO: fetch actual song names from API */}
+                                <ListItemText>{song.name || `song_${mixtape.songs[index]}`}</ListItemText>
+                              </div>
+                            </Grid>
+                            <Grid item xs={2}>
+                              <PlayIcon fontSize="large" onClick={() => playSong(index)} style={{display: isEditing ? 'none' : '' }} />
+                            </Grid>
+                          </ListItem>
+                        </Grid>
+                      )}
+                    </Draggable>
+                  ))}
+                  {provided.placeholder}
+                </List>
+              )}
+            </Droppable>
+          </DragDropContext>
+        </Grid>
+      </Box>
     );
 }
   
--- client/src/components/pages/ViewMixtapePage.jsx
+++ client/src/components/pages/ViewMixtapePage.jsx
@@ -16,7 +16,7 @@ function ViewMixtapePage(props) {
     const [isEditing, setIsEditing] = useState(false);
 
     return (
-        <div style={{paddingTop: '1em'}}>
+        <div>
             <IconButton color="secondary" aria-label="back"  onClick={() => { goBack() }}>
                 <ArrowBackIcon/>
             </IconButton>
@@ -24,7 +24,7 @@ function ViewMixtapePage(props) {
                 
             <br/>
             
-            <Paper style={{padding: '1%', marginLeft: '5%', marginBottom: '5%', width: '50%'}}>
+            <Paper style={{height: '7em', padding: '1%', marginLeft: '5%', marginBottom: '2%', width: '50%'}}>
                 {/* {isEditing ? <TextField value={mixtape.name} /> : <h1>{mixtape.name || 'Mixtape Title'}</h1>} */}
                 <Typography variant="h2">{mixtape.name}</Typography>
                 <h4>{`Created by ${getUsername(owner)} ${mixtape.songs.length} songs, XX mins`}</h4>
