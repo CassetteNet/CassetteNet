1f3489ed736f3b2eb3dc0b3d83845ee8914e4163
Michael VanDenburgh <michael.vandenburgh@stonybrook.edu>
2020-11-07 00:25:22 -0500

    configure mailgun

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,24 +1,23 @@
 const express = require('express');
 const passport = require('passport');
 const { Mixtape, User } = require('../models');
-const crypto = require('crypto');
-const nodemailer = require('nodemailer');
-const jwt = require('jsonwebtoken');
+const { sendVerificationEmail } = require('../email/email');
+
 const router = express.Router();
 
 router.post('/signup', async (req, res) => {
     const { username, password, email } = req.body;
-
     // this new user should be an admin if there are 0 users currently
     const userCount = await User.estimatedDocumentCount();
 
-    User.register(new User({ username, email, verified: false, admin: userCount === 0 }), password, (err, user) => {
-        if (err) res.send(err); // TODO: error handling
-        else{
-            
-        passport.authenticate('local')(req, res, () => res.send(user));
+    User.register(new User({ username, email, verified: false, admin: userCount === 0 }), password, async (err, user) => {
+        if (err) return res.status(500).send(err); // TODO: error handling
+        try {
+            await sendVerificationEmail(email);
+        } catch(err) {
+            console.log(err); // TODO: error handling    
         }
-        
+        passport.authenticate('local')(req, res, () => res.send(user));
     });
 });
 
