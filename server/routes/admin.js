c9e6e13a9722747d232bfa6fdac173c6cbd300ce
Michael VanDenburgh <michael.vandenburgh@stonybrook.edu>
2020-11-09 00:52:27 -0500

    update admin test db functionality

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -8,19 +8,22 @@ const router = express.Router();
 // TODO: secure this route
 router.post('/insertTestData', async (req, res) => {
     const { inboxMessages, mixtapes, users } = await generateTestData();
-    await Promise.all([...users.map(user => User.register({ username: user.username, email: user.email, _id: mongoose.Types.ObjectId(user._id) }, user.password)), InboxMessage.insertMany(inboxMessages), Mixtape.insertMany(mixtapes)]);
-    for (const user of users) {
-        const u = await User.findOne({ username: user.username }); 
-        if (!u) continue;
-        u.favoritedMixtapes = user.favoritedMixtapes;
-        u.followedUsers = user.followedUsers;
-        u.save();
-    }
+    await Promise.all([...users.map(user => User.register({ _id: mongoose.Types.ObjectId(user._id), username: user.username, email: user.email, favoritedMixtapes: user.favoritedMixtapes, followedUsers: user.followedUsers }, user.password)), InboxMessage.insertMany(inboxMessages), Mixtape.insertMany(mixtapes)]);
     res.json({
         inboxMessages,
         mixtapes,
         users,
     });
+});
+
+router.post('/dropDatabase', async (req, res) => {
+    try {
+        const conn = mongoose.connection;
+        await conn.dropDatabase();
+        res.send('database dropped.');
+    } catch (err) {
+        res.status(500).send(err);
+    }
 });
 
 
--- server/routes/user.js
+++ server/routes/user.js
@@ -66,10 +66,10 @@ router.get('/mixtapes', async (req, res) => {
 
 router.get('/favoritedMixtapes', async (req, res) => {
     if (!req.user) return res.status(401).send([]);
-    const { favoritedMixtapes } = await User.findById(req.user.id);
+    const { favoritedMixtapes } = await User.findOne({ _id: req.user.id });
     const mixtapes = [];
     for (const mixtapeId of favoritedMixtapes) {
-        const mixtape = await Mixtape.findById(mixtapeId);
+        const mixtape = await Mixtape.findOne({ _id: mixtapeId });
         if (mixtape) {
             mixtapes.push(mixtape);
         }
@@ -80,7 +80,7 @@ router.get('/favoritedMixtapes', async (req, res) => {
 router.put('/favoriteMixtape', async (req, res) => {
     if (!req.user) return res.status(401).send(null);
     const { id } = req.body;
-    const user = await User.findById(req.user._id);
+    const user = await User.findOne({ _id: req.user._id });
     if (!user.favoritedMixtapes.includes(id)) {
         user.favoritedMixtapes.push(id);
         await user.save();
@@ -91,7 +91,7 @@ router.put('/favoriteMixtape', async (req, res) => {
 router.put('/unfavoriteMixtape', async (req, res) => {
     if (!req.user) return res.status(401).send(null);
     const { id } = req.body;
-    const user = await User.findById(req.user._id);
+    const user = await User.findOne({ _id: req.user._id });
     if (user.favoritedMixtapes.includes(id)) {
         user.favoritedMixtapes.splice(user.favoritedMixtapes.indexOf(id), 1);
         await user.save();
