c7e585eb403aa8211a8c60c360efa001bb1888e8
Michael VanDenburgh <michael.vandenburgh@stonybrook.edu>
2020-11-16 00:05:34 -0500

    don't clear autocomplete onblur

--- client/src/components/SongSearchBar.jsx
+++ client/src/components/SongSearchBar.jsx
@@ -6,29 +6,34 @@ import { Autocomplete } from '@material-ui/lab';
 
 
 function SongSearchBar(props) {
-    const [open, setOpen] = useState(false);
-    const [options, setOptions] = useState([]);
-    const [searchQuery, setSearchQuery] = useState('');
-    const [loading, setLoading] = useState(false);
+  const [open, setOpen] = useState(false);
+  const [options, setOptions] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [loading, setLoading] = useState(false);
 
-    const { apiToUse, setSelected } = props;
+  const { apiToUse, setSelected } = props;
 
-    useEffect(() => {
-        if (!searchQuery) return; // don't bother calling youtube api if search is empty
-        setLoading(true); // make loading circle appear
-        songSearch(apiToUse, searchQuery)
-            .then(res => {
-                setOptions(res);
-                setLoading(false);
-            })
-            .catch(err => alert(err));
-    }, [searchQuery]);
+  useEffect(() => {
+    if (!searchQuery) return; // don't bother calling youtube api if search is empty
+    setLoading(true); // make loading circle appear
+    songSearch(apiToUse, searchQuery)
+      .then(res => {
+        setOptions(res);
+        setLoading(false);
+        console.log(res)
+      })
+      .catch(err => alert(err));
+  }, [searchQuery]);
 
-    const search = (e) => {
-        if (e.target.value) {
-            setSearchQuery(e.target.value);
-        }
+  const search = (e) => {
+    if (e.target.value) {
+      setSearchQuery(e.target.value);
     }
+  }
+
+  const blurHandler = () => {
+    setLoading(false);
+  }
 
   return (
     <Autocomplete
@@ -40,11 +45,12 @@ function SongSearchBar(props) {
       getOptionLabel={(option) => option.name}
       options={options}
       loading={loading}
+      clearOnBlur={false}
       renderInput={(params) => (
         <TextField
           {...params}
           onKeyPress={() => setLoading(true)}
-          onBlur={() => setLoading(false)}
+          onBlur={blurHandler}
           onChange={debounce((e) => search(e), 500)} // debounce every 700ms to prevent calling api more than needed
           label="Search"
           variant="outlined"
