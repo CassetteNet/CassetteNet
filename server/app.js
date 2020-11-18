20308756e240e13581e211109299a59aa280cf31
Michael VanDenburgh <michael.vandenburgh@stonybrook.edu>
2020-11-17 23:41:28 -0500

    prefix server endpoints w/ `/api`

--- server/app.js
+++ server/app.js
@@ -52,13 +52,13 @@ app.use(session({ // initialize login sessions
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use('/admin', adminRoute);
-app.use('/auth', authRoute);
-app.use('/listeningroom', listeningRoomRoute);
-app.use('/mixtape', mixtapeRoute);
-app.use('/soundcloud', soundcloudRoute);
-app.use('/user', userRoute);
-app.use('/youtube', youtubeRoute);
+app.use('/api/admin', adminRoute);
+app.use('/api/auth', authRoute);
+app.use('/api/listeningroom', listeningRoomRoute);
+app.use('/api/mixtape', mixtapeRoute);
+app.use('/api/soundcloud', soundcloudRoute);
+app.use('/api/user', userRoute);
+app.use('/api/youtube', youtubeRoute);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}...`));
