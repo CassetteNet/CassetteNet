0f8707fc689aa95fd29e705aa94f7c4dab7981a0
Michael VanDenburgh <michael.vandenburgh@stonybrook.edu>
2020-12-03 00:10:06 -0500

    add page # to query params for search results

--- client/src/components/pages/UserSearchResultsPage.jsx
+++ client/src/components/pages/UserSearchResultsPage.jsx
@@ -1,24 +1,29 @@
 import React, { useEffect, useState } from 'react';
 import { Paper, Typography } from '@material-ui/core';
 import { Pagination } from '@material-ui/lab';
+import { useHistory } from 'react-router-dom';
 import UserList from '../UserList';
 import { userSearch } from '../../utils/api';
 
 function UserSearchResults(props) {
-    const [users, setUsers] = useState(null);
+    const history = useHistory();
 
-    const [currentPage, setCurrentPage] = useState(1);
+    const [users, setUsers] = useState(null);
+    const [currentPage, setCurrentPage] = useState(new URLSearchParams(props.location.search).get('page') || 1);
     const [totalPages, setTotalPages] = useState(1);
     const [totalResults, setTotalResults] = useState(0);
 
     useEffect(() => {
-        console.log('sjdiof')
         userSearch(new URLSearchParams(props.location.search).get('query'), currentPage)
         .then(res => {
             setUsers(res.results);
             setCurrentPage(res.currentPage);
             setTotalPages(res.totalPages);
             setTotalResults(res.totalResults);
+            history.push({
+                pathname: '/search/users',
+                search: `?query=${new URLSearchParams(props.location.search).get('query')}&page=${res.currentPage}`
+            });
         });
     }, [props.location.search, currentPage]);
 
